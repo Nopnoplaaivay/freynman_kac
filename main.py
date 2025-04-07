1cdaa9dad39936759783e2163c8ed1fa766eee4e
KhangVinh <vinhkhang378@gmail.com>
2025-04-08 05:47:43 +0700

    first commit

--- main.py
+++ main.py
@@ -21,13 +21,13 @@ from src.methods import (
 
 METHODS = {
     "Monte Carlo": monte_carlo_price,
-    # "Finite Difference": finite_difference_price,
-    # "Method of Lines": method_of_lines_price,
-    # "Spectral Method": spectral_method_price,
-    # "Finite Elements": fem_price,
-    # "Deep Ritz": deep_ritz_price,  
-    # "DeepONet": deeponet_price,
-    # "Fourier Neural Operator": fourier_neural_operator_price,
+    "Finite Difference": finite_difference_price,
+    "Method of Lines": method_of_lines_price,
+    "Spectral Method": spectral_method_price,
+    "Finite Elements": fem_price,
+    "Deep Ritz": deep_ritz_price,  
+    "DeepONet": deeponet_price,
+    "Fourier Neural Operator": fourier_neural_operator_price,
     "Quantum Simulation": quantum_price_simulation  
 }
 
