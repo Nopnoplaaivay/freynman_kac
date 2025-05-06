19b43b5fbc273c1836003b30a3d605a6b10a0e55
Nopnoplaaivay <vinhkhang378@gmail.com>
2025-05-06 13:27:16 +0700

    add benchmark

--- main.py
+++ main.py
@@ -24,9 +24,9 @@ METHODS = {
     "Method of Lines": method_of_lines_price,
     "Spectral Method": spectral_method_price,
     "Finite Elements": fem_price,
-    "Deep Ritz": deep_ritz_price,
-    "DeepONet": deeponet_price,
-    "Fourier Neural Operator": fourier_neural_operator_price,
+    # "Deep Ritz": deep_ritz_price,
+    # "DeepONet": deeponet_price,
+    # "Fourier Neural Operator": fourier_neural_operator_price,
     "Quantum Simulation": quantum_price_simulation
 }
 
